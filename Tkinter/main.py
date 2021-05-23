a79fd7cff0133044aed26a56abb42c41ef57b332
Rishi Jani <rishi.jani2019@gmail.com>
2021-05-23 16:29:19 +0530

    Create main.py

---
+++ Tkinter/main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+root.
+
+label = Label(root, text="Hello Khushal")
+label.pack()
+
+
+cont = Button(root, text="Let's Continue")
+cont.pack()
+
+root.mainloop()
